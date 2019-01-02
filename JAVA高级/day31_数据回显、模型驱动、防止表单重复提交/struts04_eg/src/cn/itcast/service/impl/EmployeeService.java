962690bb7ab6c9657bf90124473b966c24feabb4
15529343201 <15529343201@139.com>
2019-01-02 20:17:24 +0800

    add day31

---
+++ JAVA高级/day31_数据回显、模型驱动、防止表单重复提交/struts04_eg/src/cn/itcast/service/impl/EmployeeService.java
@@ -0,0 +1,49 @@
+package cn.itcast.service.impl;
+
+import java.util.List;
+
+import cn.itcast.dao.IEmployeeDao;
+import cn.itcast.dao.impl.EmployeeDao;
+import cn.itcast.entity.Employee;
+import cn.itcast.service.IEmployeeService;
+
+public class EmployeeService implements IEmployeeService{
+	
+	private IEmployeeDao employeeDao = new EmployeeDao();
+
+	public Employee findById(int id) {
+		try {
+			return employeeDao.findById(id);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public List<Employee> getAll() {
+		try {
+			return employeeDao.getAll();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public void save(Employee emp) {
+		try {
+			employeeDao.save(emp);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		
+	}
+
+	public void update(Employee emp) {
+		try {
+			employeeDao.update(emp);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		
+		
+	}
+
+}
