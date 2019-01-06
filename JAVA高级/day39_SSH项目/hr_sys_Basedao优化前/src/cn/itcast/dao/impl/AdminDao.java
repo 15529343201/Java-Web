3476f8aee79fc49579a74e5495f4d1e19da04950
15529343201 <15529343201@139.com>
2019-01-06 19:42:43 +0800

    add day39_ssh

---
+++ JAVA高级/day39_SSH项目/hr_sys_Basedao优化前/src/cn/itcast/dao/impl/AdminDao.java
@@ -0,0 +1,28 @@
+package cn.itcast.dao.impl;
+
+import org.hibernate.SessionFactory;
+import cn.itcast.dao.IAdminDao;
+import cn.itcast.entity.Admin;
+
+public class AdminDao implements IAdminDao{
+
+	private SessionFactory sessionFactory;
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	@Override
+	public Admin findByAdmin(Admin admin) {
+		return (Admin) sessionFactory.getCurrentSession()//
+				.createQuery("from Admin where adminName=? and pwd=?")//
+				.setString(0, admin.getAdminName())//
+				.setString(1, admin.getPwd())//
+				.uniqueResult();
+	}
+
+	@Override
+	public void save(Admin admin) {
+		sessionFactory.getCurrentSession().save(admin);
+	}
+
+}
